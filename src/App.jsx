ff9cc488b008c93d78847677e1d49c13a7ff32b4
Rohan <rohan151025@gmail.com>
2026-02-01 10:14:23 +0530

    Start the project with css reset and other template stuff

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GlobalStyles } from './GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <strong>Blank</strong> <strong>Slate</strong>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
